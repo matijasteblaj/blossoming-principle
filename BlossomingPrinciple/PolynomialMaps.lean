d0582582e84f659a1d66359f3b41ce429c0e0dd5
Matija Šteblaj <matija.steblaj@gmail.com>
2025-08-06 17:13:20 +0200

    Stating intermediate lemmas

--- BlossomingPrinciple/PolynomialMaps.lean
+++ BlossomingPrinciple/PolynomialMaps.lean
@@ -62,10 +62,73 @@ def isPolyMapInCoords (map : P → R) (cs₁ : CoordinateSystem ι F V P)
 
   variable {ι₁ ι₂ κ₁ κ₂ : Type}
 
+lemma poly_change_codomain_origin (map : P → R) (new_origin : R)
+ {cs₁ : CoordinateSystem ι₁ F V P}
+ {cs₂ : CoordinateSystem κ₁ F W R}:
+    let cs₄ : CoordinateSystem κ₁ F W R := {cs₂ with origin := new_origin}
+    (isPolyMapInCoords map cs₁ cs₂) → (isPolyMapInCoords map cs₁ cs₄) := by
+    intro cs₄ h
+    unfold isPolyMapInCoords at *
+    rcases h with ⟨M⟩
+    constructor
+    let γ := cs₂.basis.repr (cs₂.origin -ᵥ new_origin)
+    let new_polys (i : κ₁) := M.polys i + MvPolynomial.C (γ i)
+    use new_polys
+    intro p i
+    simp [new_polys]
+    rw [← M.map_poly_eq p i]
+    unfold CoordinateSystem.coordinates at *
+    simp [cs₄, γ]
+    rw [← vsub_add_vsub_cancel (map p) cs₂.origin new_origin, LinearEquiv.map_add]
+    rfl
+
+lemma poly_change_domain_origin (map : P → R) (new_origin : P)
+ {cs₁ : CoordinateSystem ι₁ F V P}
+ {cs₂ : CoordinateSystem κ₁ F W R}:
+    let cs₃ : CoordinateSystem ι₁ F V P := {cs₁ with origin := new_origin}
+    (isPolyMapInCoords map cs₁ cs₂) → (isPolyMapInCoords map cs₃ cs₂) := by
+    intro cs₃ h
+    unfold isPolyMapInCoords at *
+    rcases h with ⟨M⟩
+    constructor
+    let γ := cs₁.basis.repr (cs₁.origin -ᵥ new_origin)
+    sorry
+    /-
+    let new_polys (i : κ₁) := M.polys i + MvPolynomial.C (γ i)
+    use new_polys
+    intro p i
+    simp [new_polys]
+    rw [← M.map_poly_eq p i]
+    unfold CoordinateSystem.coordinates at *
+    simp [cs₄, γ]
+    rw [← vsub_add_vsub_cancel (map p) cs₂.origin new_origin, LinearEquiv.map_add]
+    rfl-/
+
+lemma poly_change_codomain_basis (map : P → R)
+  (new_basis : Module.Basis κ₂ F W)
+  {cs₁ : CoordinateSystem ι₁ F V P}
+  {cs₂ : CoordinateSystem κ₁ F W R}:
+    let cs₄ : CoordinateSystem κ₂ F W R := {origin := cs₂.origin, basis := new_basis}
+    (isPolyMapInCoords map cs₁ cs₂) → (isPolyMapInCoords map cs₁ cs₄) := by
+    sorry
+
+lemma poly_change_domain_basis (map : P → R)
+  (new_basis : Module.Basis ι₂ F V)
+  {cs₁ : CoordinateSystem ι₁ F V P}
+  {cs₂ : CoordinateSystem κ₁ F W R}:
+    let cs₃ : CoordinateSystem ι₂ F V P := {origin := cs₁.origin, basis := new_basis}
+    (isPolyMapInCoords map cs₁ cs₂) → (isPolyMapInCoords map cs₃ cs₂) := by
+    sorry
+
+
+
 theorem poly_in_one_poly_in_another (map : P → R) (cs₁ : CoordinateSystem ι₁ F V P)
  (cs₂ : CoordinateSystem κ₁ F W R) (cs₃ : CoordinateSystem ι₂ F V P)
  (cs₄ : CoordinateSystem κ₂ F W R):
    (isPolyMapInCoords map cs₁ cs₂) → (isPolyMapInCoords map cs₃ cs₄) := by
+   intro h
+   unfold isPolyMapInCoords
+   constructor
    sorry
 
 theorem poly_in_one_poly_in_all (map : P → R) (cs₁ : CoordinateSystem ι F V P)
@@ -78,11 +141,13 @@ theorem poly_in_one_poly_in_all (map : P → R) (cs₁ : CoordinateSystem ι F V
 
 def isPolyMap
   (map: P → R): Prop :=
-  ∃ (cs₁ : CoordinateSystem ι F V P) (cs₂ : CoordinateSystem κ F W R),
+  ∀ {F V W ι κ : Type} [Field F]
+    [AddCommGroup V] [VectorSpace F V] [AffineSpace V P]
+    [AddCommGroup W] [VectorSpace F W] [AffineSpace W R]
+    (cs₁ : CoordinateSystem ι F V P) (cs₂ : CoordinateSystem κ F W R),
     isPolyMapInCoords map cs₁ cs₂
 
-
-theorem affine_map_is_poly_deg_1
+theorem affine_map_is_poly
   (ϕ : AffineMap F P R):
   isPolyMap ϕ.toFun := by
   sorry
